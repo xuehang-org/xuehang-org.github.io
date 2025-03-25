9b69def9c9750953875f359d25465f6ed0364339
XuehangCang <xuehangcang@outlook.com>
2025-03-25 14:10:15 +0800

    update:vite

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
         vueDevTools(),
         tailwindcss(),
     ],
+    base: './',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
