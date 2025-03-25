07f37a13785b303fe8b2cde3273da6bbd7e4feb1
XuehangCang <xuehangcang@outlook.com>
2025-03-25 14:23:24 +0800

    update:vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
         tailwindcss(),
     ],
-    base: 'https://xuehang.org/',
+    base: './',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
